d9eff0c212d7c680acfc96141364c865863e3076
eureka857 <tianheshen@zju.edu.cn>
2024-10-26 07:13:35 +0800

    Update app4D-Copy1.py

--- app4D-Copy1.py
+++ app4D-Copy1.py
@@ -75,7 +75,7 @@ st.write(input_df)
 
 # 做预测
 if st.button('点击进行预测'):
-    output1, output2, output3 = predict(input_df)
+      output1, output2, output3 = predict(input_df)
     
     # 显示预测结果的结构
     st.subheader('FEV1/FVC 预测结果')
@@ -102,11 +102,11 @@ try:
            st.warning("您目前很可能患有慢阻肺，请进一步行肺功能检查。")
        else:
            # GOLDCOPD_score 为 0 时，显示进一步的分析结果
-           if fev1_fvc_score < 72 and fev1_pred_score < 78:
+           if fev1_fvc_score < 71 and fev1_pred_score < 78:
                st.error("您可能患有中度及以上慢阻肺，请立即联系呼吸专科医生。")
            elif 72 <= fev1_fvc_score < 80:
                st.warning("您目前还不是慢阻肺，但有患上慢阻肺的风险，请戒烟，增加体重，加强锻炼，参加肺功能筛查测试或纳入年度体检计划。")
-           elif fev1_fvc_score >= 72 and fev1_pred_score <= 78:
+           elif fev1_fvc_score >= 71 and fev1_pred_score <= 78:
                st.warning("您可能存在保留比值肺功能受损，请关注您的呼吸健康情况，建议进一步行肺功能筛查测试。")
            elif fev1_fvc_score <= 69:
                st.warning("您可能存在阻塞性通气功能障碍，请关注您的呼吸健康情况，建议进一步行肺功能筛查测试。")
